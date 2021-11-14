ed7e888ba314ac24b1daa66519f0969fe5436fb0
hoshianaaa <hoshina12345@gmail.com>
2021-11-14 11:29:21 +0900

    bufferのデータがちゃんと取れているか確認しないと映像が割れる.

--- src/stream_ros.cpp
+++ src/stream_ros.cpp
@@ -9,6 +9,54 @@
 #include <image_transport/image_transport.h>
 #include <camera_info_manager/camera_info_manager.h>
 
+#include <png.h> // Requires libpng1.2
+#include <assert.h>
+
+/**
+ * Reads image data from an Aravis ArvBuffer and saves a png file to filename
+ * TODO: Add error checking and all that stuff (this code is demonstrative)
+ */
+void arv_save_png(ArvBuffer * buffer, const char * filename)
+{
+	// TODO: This only works on image buffers
+	assert(arv_buffer_get_payload_type(buffer) == ARV_BUFFER_PAYLOAD_TYPE_IMAGE);
+
+	size_t buffer_size;
+	char * buffer_data = (char*)arv_buffer_get_data(buffer, &buffer_size); // raw data
+
+	int width; int height;
+	arv_buffer_get_image_region(buffer, NULL, NULL, &width, &height); // get width/height
+	int bit_depth = ARV_PIXEL_FORMAT_BIT_PER_PIXEL(arv_buffer_get_image_pixel_format(buffer)); // bit(s) per pixel
+	//TODO: Deal with non-png compliant pixel formats?
+	// EG: ARV_PIXEL_FORMAT_MONO_14 is 14 bits per pixel, so conversion to PNG loses data
+
+	int arv_row_stride = width * bit_depth/8; // bytes per row, for constructing row pointers
+	int color_type = PNG_COLOR_TYPE_GRAY; //TODO: Check for other types?
+
+	// boilerplate libpng stuff without error checking (setjmp? Seriously? How many kittens have to die?)
+	png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	png_infop info_ptr = png_create_info_struct(png_ptr);
+	FILE * f = fopen(filename, "wb");
+	png_init_io(png_ptr, f);
+	png_set_IHDR(png_ptr, info_ptr, width, height, bit_depth, color_type,
+		PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+	png_write_info(png_ptr, info_ptr);
+
+	// Need to create pointers to each row of pixels for libpng
+	png_bytepp rows = (png_bytepp)(png_malloc(png_ptr, height*sizeof(png_bytep)));
+	int i =0;
+	for (i = 0; i < height; ++i)
+		rows[i] = (png_bytep)(buffer_data + (i)*arv_row_stride);
+	// Actually write image
+	png_write_image(png_ptr, rows);
+	png_write_end(png_ptr, NULL); // cleanup
+	png_free(png_ptr, rows);
+	png_destroy_write_struct(&png_ptr, &info_ptr);
+	fclose(f);
+}
+
+
+
 image_transport::Publisher publisher;
 ros::NodeHandle *node_handle;
 ros::Time p_cb_time;
@@ -37,6 +85,8 @@ new_buffer_cb (ArvStream *stream, ApplicationData *data)
 	buffer = arv_stream_try_pop_buffer (stream);
 	if (buffer != NULL) {
 		if (arv_buffer_get_status (buffer) == ARV_BUFFER_STATUS_SUCCESS)
+    {
+      arv_save_png(buffer, "image.png");
 			data->buffer_count++;
 		/* Image processing here */
 
@@ -50,6 +100,20 @@ new_buffer_cb (ArvStream *stream, ApplicationData *data)
         std::cout << "buffer size:" << buffer_size << std::endl;
         std::vector<uint8_t> this_data(buffer_size);
         memcpy(&this_data[0], buffer_data, buffer_size);
+
+        std::cout << "data:";
+        for(int i=0;i<buffer_size;i++)
+        {
+          std::cout << " " << int(buffer_data[i]);
+        }
+        std::cout << std::endl;
+
+        std::cout << "data2:";
+        for(int i=0;i<buffer_size;i++)
+        {
+          std::cout << " " << int(this_data[i]);
+        }
+        std::cout << std::endl;
 
         sensor_msgs::Image msg;
         msg.header.stamp = ros::Time::now(); // host timestamps (else buffer->timestamp_ns)
@@ -69,6 +133,7 @@ new_buffer_cb (ArvStream *stream, ApplicationData *data)
         if (delay < 0.001)delay=0;
 
         std::cout << "pub delay[/s]:" << delay << std::endl;
+    }
 
 	}
 }
@@ -124,7 +189,7 @@ main (int argc, char **argv)
 		gint payload;
 
 		/* Set region of interrest to a 200x200 pixel area */
-		//arv_camera_set_region (camera, 0, 0, 200, 200, NULL);
+//		arv_camera_set_region (camera, 0, 0, 2, 3, NULL);
     gint x,y;
     arv_camera_get_region (camera, &x, &y, &width, &height, NULL);
 		/* Set frame rate to 10 Hz */
